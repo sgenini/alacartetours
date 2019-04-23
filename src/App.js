59a29a41170aa18b2d181a6589912a6082a75f01
Stephane Genini <sgenini@gmail.com>
2019-04-22 19:42:02 -0700

    Pre-commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './assets/styles/app.css';
 
-import Default from "./components/Default/Default"
+import Default from "./components/Default/Default";
+import Navbar from "./components/Navbar/Navbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <Default />
       </div>
     );
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbarComponent">
+        <p>Under construction, check back soon! <span role="img" aria-label="fight-on">✌</span></p>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
