6186243e05c1e13cebd3c97ae2bf60afc68ed9bc
Stephane Genini <sgenini@gmail.com>
2019-04-22 18:46:24 -0700

    file restructure, default component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/app.css';
+
+import Default from "./components/Default/Default"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Website currently under construction. 
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Default />
       </div>
     );
   }
---
+++ src/components/Default/Default.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+class Default extends Component {
+  render() {
+    return (
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Under construction, check back soon! <span role="img" aria-label="fight-on">✌</span></p>
+      </div>
+    );
+  }
+}
+
+export default Default;
